d2bb97f03e01cc76e7577bbe72f08a60866f3085
eigil <ek@lundbrandhouse.dk>
2020-10-28 22:39:33 +0100

    init

--- App.js
+++ App.js
@@ -1,18 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+
+    /*You can use component didmount*/
+    constructor() {
+        super();
+        this.fetchData();
+    }
+
+    state={
+        wehlersProducts:{}
+    }
+
+    /*Asynkront kald*/
+    fetchData = async () =>{
+
+        const url = 'http://13.69.31.213/wh/getall';
+
+        try {
+            /*Try'er to we can fetch, cant fetch on browser bcs cors*/
+          const response =  await fetch(url);
+          /*Await to parse when we have const response*/
+          const jsonResponse = await response.json();
+          /*Get JSON and parse*/
+          const parsed = JSON.parse(jsonResponse.response);
+          console.log("\n\n ------Results-----\n");
+          const wehlersArray=[];
+
+        /*Remember to spell Whelers correct from DB product 1 and 13 */
+          for(let [index, product] of Object.entries(parsed)){
+
+              if(product.Record.product_brand === "Wehlers" || product.Record.product_brand === "Whelers" ){
+
+                  product.Record.metadata = JSON.parse(product.Record.metadata)
+
+                  wehlersArray.push(product)
+              }
+          }
+
+          this.setState({wehlersProducts:wehlersArray})
+
+            /*Amount of products */
+          console.log("Efter push til array antal:\n",wehlersArray.length);
+
+          /*Use this one to output wehlers products */
+
+          console.log("Wehlers produkter:\n",wehlersArray);
+
+        }catch (e) {
+          console.log("\n---FejlBesked---\n\n",e)
+        }
+    };
+
+  render(){
+    const {wehlersProducts} = this.state;
+
+    return(
+        <View style={styles.container}>
+            <FlatList
+            data={wehlersProducts}
+            keyExtractor={item => item.Key}
+            renderItem={({item})=>(
+                <View>
+                    <Text>Key: {item.Key}  </Text>
+                    <Text>Product_brand: {item.Record.product_brand} {"\n"}</Text>
+                </View>
+
+            )}
+            />
+        </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
+      padding:50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
