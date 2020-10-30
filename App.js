9b7c7939b532161c1cf2b9e02cba81f33fc2dcc0
eigilak <hello@eigilak.dk>
2020-10-30 13:12:44 +0100

    Update

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,FlatList } from 'react-native';
+import { StyleSheet, Text, View,FlatList,Platform } from 'react-native';
 
 export default class App extends React.Component{
 
-    /*You can use component didmount*/
-    constructor() {
-        super();
+    componentDidMount() {
         this.fetchData();
     }
 
@@ -16,42 +14,47 @@ export default class App extends React.Component{
 
     /*Asynkront kald*/
     fetchData = async () =>{
+        if(Platform.OS !== "web"){
+            const url = 'http://13.69.31.213/wh/getall';
 
-        const url = 'http://13.69.31.213/wh/getall';
+            try {
+                /*Try'er to we can fetch, cant fetch on browser bcs cors*/
+                const response =  await fetch(url);
+                /*Await to parse when we have const response*/
+                const jsonResponse = await response.json();
+                /*Get JSON and parse*/
+                const parsed = JSON.parse(jsonResponse.response);
+                console.log("\n\n ------Results-----\n");
+                const wehlersArray=[];
 
-        try {
-            /*Try'er to we can fetch, cant fetch on browser bcs cors*/
-          const response =  await fetch(url);
-          /*Await to parse when we have const response*/
-          const jsonResponse = await response.json();
-          /*Get JSON and parse*/
-          const parsed = JSON.parse(jsonResponse.response);
-          console.log("\n\n ------Results-----\n");
-          const wehlersArray=[];
+                /*Remember to spell Whelers correct from DB product 1 and 13 */
+                for(let [index, product] of Object.entries(parsed)){
 
-        /*Remember to spell Whelers correct from DB product 1 and 13 */
-          for(let [index, product] of Object.entries(parsed)){
+                    if(product.Record.product_brand === "Wehlers" || product.Record.product_brand === "Whelers" ){
 
-              if(product.Record.product_brand === "Wehlers" || product.Record.product_brand === "Whelers" ){
+                        product.Record.metadata = JSON.parse(product.Record.metadata)
 
-                  product.Record.metadata = JSON.parse(product.Record.metadata)
+                        wehlersArray.push(product)
+                    }
+                }
 
-                  wehlersArray.push(product)
-              }
-          }
+                this.setState({wehlersProducts:wehlersArray})
 
-          this.setState({wehlersProducts:wehlersArray})
+                /*Amount of products */
+                console.log("Efter push til array antal:\n",wehlersArray.length);
 
-            /*Amount of products */
-          console.log("Efter push til array antal:\n",wehlersArray.length);
+                /*Use this one to output wehlers products */
 
-          /*Use this one to output wehlers products */
+                console.log("Wehlers produkter:\n",wehlersArray);
 
-          console.log("Wehlers produkter:\n",wehlersArray);
-
-        }catch (e) {
-          console.log("\n---FejlBesked---\n\n",e)
+            }catch (e) {
+                console.log("\n---FejlBesked---\n\n",e)
+            }
+        }else {
+            alert('Denne applikation er kun til mobile enheder!')
         }
+
+
     };
 
   render(){
@@ -65,6 +68,19 @@ export default class App extends React.Component{
             renderItem={({item})=>(
                 <View>
                     <Text>Key: {item.Key}  </Text>
+
+                    {/*Meta data loop*/}
+                    <FlatList
+                        data={item.Record.metadata}
+                        keyExtractor={item => item.event_timestamp}
+                        renderItem={({item})=> (
+                            <Text>
+                              Meta Event src: {item.event_src}
+                            </Text>
+                        )}
+                    />
+                    <Text>Produkt id:  {item.Record.product_id}</Text>
+                    <Text>Product time: {item.Record.product_time}</Text>
                     <Text>Product_brand: {item.Record.product_brand} {"\n"}</Text>
                 </View>
 
